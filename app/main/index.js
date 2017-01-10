91330fbaa5cccb00ec6e1bd1bac9e979cc6228e9
wuyefenglan <13163139273@163.com>
2017-01-10 17:13:12 +0800

    简单测试

--- app/main/index.js
+++ app/main/index.js
@@ -1,3 +1,12 @@
-/**
- * Created by Administrator on 2017/1/10 0010.
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Toggle from "../components/Toggle/Toggle.js";
+
+class Main extends React.Component {
+    render(){
+        return (
+            <Toggle/>
+        );
+    }
+}
+ReactDOM.render(<Main/>,document.getElementById("container"));
