66c1054e5332cf927ed348d9c94978e9a9dcf816
wuyefenglan <13163139273@163.com>
2017-01-10 17:12:52 +0800

    简单测试

--- app/components/Toggle/Toggle.js
+++ app/components/Toggle/Toggle.js
@@ -1,3 +1,24 @@
-/**
- * Created by Administrator on 2017/1/10 0010.
- */
+import React,{Component} from 'react';
+
+export default class Toggle extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isToggleOn:true
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(){
+        this.setState(prevState =>({
+            isToggleOn:!prevState.isToggleOn
+        }))
+    }
+
+    render(){
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn?"ON":"OFF"}
+            </button>
+        );
+    }
+}
